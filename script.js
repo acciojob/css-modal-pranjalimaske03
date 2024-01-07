c3b9f4d94b00c87ba7093334b8c2d2189a856b5f
Pranjali Maske <110550905+pranjalimaske03@users.noreply.github.com>
2024-01-07 11:29:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// Get the modal and the button
+var modal = document.getElementById('myModal');
+var btn = document.getElementById('openModal');
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
