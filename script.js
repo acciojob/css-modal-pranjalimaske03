190766cae1da3b005de3bb07cb1269ca246a2ecd
Pranjali Maske <110550905+pranjalimaske03@users.noreply.github.com>
2024-01-12 11:03:57 +0530

    created a submission

--- script.js
+++
@@ -1,8 +0,0 @@
-// Open modal
-cy.get("#openModal").click();
-
-// Click the close button inside the modal
-cy.get(".close-modal").click();
-
-// Verify that the modal is closed
-cy.get(".modal").should("have.css", "display", "none");
